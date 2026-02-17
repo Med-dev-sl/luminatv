15c86e45cecc51ea451802112c4d912a7bf6a812
123paulsallu <magbieprincess@gmail.com>
2026-02-17 19:15:55 +0000

    Secure settings for production

--- luminatv/luminatv/settings.py
+++ luminatv/luminatv/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xzbb)s!ubz4k1a4sv5!r(57mtd9^h1by2&64d_x9j%%q%w*kl*'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-xzbb)s!ubz4k1a4sv5!r(57mtd9^h1by2&64d_x9j%%q%w*kl*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
