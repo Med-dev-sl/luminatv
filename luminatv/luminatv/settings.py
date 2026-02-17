153992084d96a24dcceb07d425e6daec0942d2aa
123paulsallu <magbieprincess@gmail.com>
2026-02-17 19:14:27 +0000

    Configure Render deployment files

--- luminatv/luminatv/settings.py
+++ luminatv/luminatv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xzbb)s!ubz4k1a4sv5!r(57mtd9^h1by2&64d_x9j%%q%w*kl*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
